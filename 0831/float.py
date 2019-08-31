cc4bf8b3e1483029c4ce563434b28bc4d9ef0e88
synapse99 <49575952+synapse99@users.noreply.github.com>
2019-08-31 11:06:00 +0900

    Add files via upload

---
+++ 0831/float.py
@@ -0,0 +1,6 @@
+print("{0:.3}".format(9.3/2.7))
+y = 2.5 * 4.8
+print("{:.1f}" .format(y))
+r = 8/float(3)
+print("{:.2f}".format(r))
+print("{0:.4}".format(8.0/3))
---
+++ 0831/integer.py
@@ -0,0 +1,13 @@
+print(1)
+x = 9
+print("{0}".format(x))
+print("{}" .format(3**4))
+print("{}" .format(int(9.3)/int(2.7)))
+
+
+print("{0:.3}".format(9.3/2.7))
+y = 2.5 * 4.8
+print("{:.1f}" .format(y))
+r = 8/float(3)
+print("{:.2f}".format(r))
+print("{0:.4}".format(8.0/3))
