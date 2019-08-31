7cbc2b1a9867095ec6f13efd77f0a95bdeb09a14
synapse99 <49575952+synapse99@users.noreply.github.com>
2019-08-31 12:39:59 +0900

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+print("{:s}".format('I\'m enjoying learning Python.'))
+print("{:s}".format("This is a long string. Without the backslash\
+it would run off of the page on the right in the text editor and be very\
+difficult in read and edit. By using the backslash tou con spilt the long\
+string into smaller strings on seperate lines so that the whole string is easy\
+to veiw in the editer."))
+
+print("{0:s}".format('''You con also use triple double qoutes
+for multi-line comment strings.'''))
+
+print("{:s}".format('''You con also use triple double qoutes
+for multi-line comment strings.'''))
